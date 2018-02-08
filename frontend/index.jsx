9b304b548816cd3aa433c279644feea8ba3285b5
Frankie Ziman <FNZiman@gmail.com>
2018-02-08 14:52:53 -0800

    setup HashRouter and Provider

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Util from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
 
-  ReactDOM.render(<h1>TEST</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 
-  window.signup = Util.signup;
-  window.login = Util.login;
-  window.logout = Util.logout;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-
 });
